928d78e6f0821f0648276a3f418d54a6aa9f6439
Doma Z.A <zivanaiarnolddoma@gmail.com>
2023-03-01 17:10:59 +0300

    Initial commit

---
+++ index.bundle.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log("hello world");
+console.debug("Build functional");
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7O0FBQUE7QUFDQSxrQyIsInNvdXJjZXMiOlsid2VicGFjazovL3Jlc3RhdXJhbnQtd2ViLWFwcC8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZyhcImhlbGxvIHdvcmxkXCIpO1xuY29uc29sZS5kZWJ1ZyhcIkJ1aWxkIGZ1bmN0aW9uYWxcIik7Il0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
