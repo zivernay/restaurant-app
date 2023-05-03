72b946c06ee60c288ecabb2bdaf0365c6b91d1c2
Doma Z.A <zivanaiarnolddoma@gmail.com>
2023-05-03 19:46:47 +0300

    Refactor the tab functions to work with isolated Html.

--- src/index.js
+++ src/index.js
@@ -19,62 +19,57 @@ function getDealMarkUp(info) {
     `;
     return articleMarkUp;
 }
-function createDealElement(dealData){
-    const dealElement = document.createElement("article");
-    const dealInfo = parseDealInfo(dealData);
-    const markUp = getDealMarkUp(dealInfo); // Info is parsed data
-    dealElement.innerHTML = markUp;
-    dealElement.classList.add(dealInfo.className);
-    return dealElement;
+function createDealElement(id){
+    const li = document.createElement("li");
+    const dealTemp = document.querySelector(".deal.template");
+    const deal = dealTemp.cloneNode(true);
+    deal.classList.remove("template");
+    deal.setAttribute("data-id", id);
+    li.appendChild(deal);
+    return li;
 }
 
 function showHomepage() {
+    const header = document.querySelector("#header");
     const dealsContainer = document.querySelector("#offers");
+    const featured = document.querySelector(".template.featured");
+    const intro = document.querySelector(".intro");
+    intro.classList.remove("hidden");
+    header.classList.remove("hidden");
     dealsContainer.innerHTML = ""; // clearing the container everytime
-    const generatedContent = document.createElement("div");
-    generatedContent.setAttribute("id", "featured-offers")
-    for(i=0; i<4; i++) {
-        const dealElement = createDealElement(1);
-        if(dealElement.classList.contains("featured")){
-            generatedContent.appendChild(dealElement);
-        }    
-    dealsContainer.appendChild(generatedContent);
-    }
+    const clonedFeatured = featured.cloneNode(true);
+    clonedFeatured.classList.remove(...["hidden", "template"]);
+    dealsContainer.appendChild(clonedFeatured);
 }
+
 function showMenu() {
     const dealsContainer = document.querySelector("#offers");
-    dealsContainer.innerHTML = "";
-    const generatedContent = document.createElement("div");
-    generatedContent.setAttribute("id", "offer-list");
-    for(i=0; i<9; i++) {
-        const dealElement = createDealElement(1);    
-        generatedContent.appendChild(dealElement);
+    const menuTemp = document.querySelector("#menu");
+    const deals = document.querySelector(".menu-items");
+    const intro = document.querySelector(".intro");
+    intro.classList.add("hidden");
+    dealsContainer.innerHTML = ""; 
+    for(let i=0; i<9; i++) {
+        const deal = createDealElement(i);    
+        deals.appendChild(deal);
     }
-    dealsContainer.appendChild(generatedContent);
+    const menu = menuTemp.cloneNode(true);
+    dealsContainer.appendChild(menu);
 }
+
 function showContactInformation() {
     const dealsContainer = document.querySelector("#offers");
     dealsContainer.innerHTML = "";
-    const generatedContent = document.createElement("div");
-    generatedContent.setAttribute("id", "contact-information");
-    const addressElement = document.createElement("address");
-    const instructions = document.createElement("p");
-    instructions.innerHTML = `To place an order:`;
-    addressElement.innerHTML = `
-        call us <a href="tel:+9188598670">(918) 859 86-70</a><br>
-        or <br>
-        email us <a href="mailto:order@restaurant.com">order@mail.com</a><br>
-        or <br>
-        visit any of our store:
-            23 Main Street, City 1 <br>
-            2 Priafgal av., City 1 <br>
-            CityMall 6. building 2, City 3<br>`;
-    generatedContent.appendChild(instructions);
-    generatedContent.appendChild(addressElement);
-    dealsContainer.appendChild(generatedContent);
+    const intro = document.querySelector(".intro");
+    intro.classList.add("hidden");
+    const contactTemp = document.querySelector(".contact.template");
+    const contact = contactTemp.cloneNode(true);
+    contact.classList.remove("template");
+    dealsContainer.appendChild(contact);
 }
 
 // Events 
+document.body.onload = showHomepage();
 const homeTab = document.querySelector("#home-tab");
 homeTab.addEventListener("click", (e)=> {
     showHomepage();
